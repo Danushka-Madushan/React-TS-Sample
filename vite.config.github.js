32a67712729f472333c0eb18c71158e0d9c14f1e
Danushka-Madushan <Danushkamadushan128@gmail.com>
2024-01-11 04:51:16 +0530

    Update vite.config.github.js

--- vite.config.github.js
+++ vite.config.github.js
@@ -1,7 +1,12 @@
+/* 
+  Why this file exisis in this project 
+  https://vitejs.dev/guide/static-deploy.html#github-pages
+*/
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/React-TS-Sample'
