71b3815e7b5b0d1308d2c6043767bded9c21b225
Danushka-Madushan <Danushkamadushan128@gmail.com>
2024-01-20 23:08:51 +0530

    update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,30 +4,36 @@ import { Item } from './components/Item'
 import { Items } from './constants/store'
 import { Axios } from './hooks/axios'
 import { Loader } from './components/Loader'
+import { useCookies } from 'react-cookie'
+import { UNIQUE_ID } from './config/config'
+import { v5 as uuidv5, v4 as uuidv4 } from 'uuid';
 
 const App = () => {
+  const [cookies, setCookie] = useCookies(['group'])
   const [items, setItems] = useState(Items)
   const [isLoading, setLoading] = useState(true)
 
   useEffect(() => {
+    if (!cookies.group) {
+      setCookie('group', uuidv5(uuidv4(), UNIQUE_ID), { path: '/' });
+    }
+
     const Fetch = async () => {
       setLoading(true)
       const { data: { content } } = await Axios<{ content: typeof Items }>('/', 'POST', {
-        "service": "to-do",
-        "action": "get",
-        "content": {
-          "group": "dd79cc1c-bc08-444c-bf28-a2001c88c4e6"
-        }
+        "service": "to-do", "action": "get",
+        "content": { "group": cookies.group }
       })
       setLoading(false)
       setItems(content)
     }
+
     Fetch()
-  }, [])
+  }, [cookies, setCookie])
 
   return (
     <>
-      {isLoading && <Loader/>}
+      {isLoading && <Loader />}
       {!isLoading && (
         <div className='mx-4 py-2'>
           <InsertBar setItems={setItems} />
